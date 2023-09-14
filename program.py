7a6ad8584a5741d95a0a963b598b264b02acd396
megahomyak <g.megahomyak@gmail.com>
2023-09-14 11:18:41 +0500

    Shit works *occasionally* and it sucks

--- program.py
+++ program.py
@@ -10,14 +10,13 @@ class Fnoof(QMainWindow):
         # self.setWindowFlag(Qt.WindowType.X11BypassWindowManagerHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.old_pointer_position = None
-        image_size = QPixmap("images/bodies.png").size()
-        self.win_width = 300
-        self.win_height = int(image_size.height() / image_size.width() * self.win_width)
         self.bodies = self.image_label("images/bodies.png")
         self.heads = self.image_label("images/heads.png")
         self.foreground = self.image_label("images/foreground.png")
+        width = 300
+        height = int(self.bodies.pixmap().height() / self.bodies.pixmap().width() * width)
         x, y = 0, 0
-        self.setGeometry(x, y, self.win_width, self.win_height)
+        self.setGeometry(x, y, width, height)
         self.animation = QPropertyAnimation(self.heads, b"pos")
         self.animation.setEasingCurve(QEasingCurve.Type.InOutCubic)
         self.animation.setEndValue(self.heads.pos() + QPoint(0, 100))
@@ -26,10 +25,9 @@ class Fnoof(QMainWindow):
 
     def image_label(self, path):
         label = QLabel(self)
-        label.setGeometry(0, 0, self.win_width, self.win_height)
+        label.setGeometry(0, 0, 300, 238)
         pixmap = QPixmap(path).scaled(label.size(), Qt.AspectRatioMode.KeepAspectRatio)
         label.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
-        pixmap = QPixmap(path)
         label.setPixmap(pixmap)
         return label
 
