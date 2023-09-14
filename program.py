11ea57003c2be823e3b3095055ce521e5330e3b2
megahomyak <g.megahomyak@gmail.com>
2023-09-14 10:26:19 +0500

    Before deleting `paintEvent`

--- program.py
+++ program.py
@@ -1,19 +1,39 @@
-from PyQt6.QtCore import QPoint, Qt
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import QEasingCurve, QPoint, QPropertyAnimation, Qt
+from PyQt6.QtGui import QPainter, QPixmap
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
-
-def load_image(path):
-    label = QLabel()
-    label.setPixmap(QPixmap(path))
-    return label
 
 class Fnoof(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
+        # self.setWindowFlag(Qt.WindowType.X11BypassWindowManagerHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.old_pointer_position = None
+        self.bodies = self.image_label("images/bodies.png")
+        self.heads = self.image_label("images/heads.png")
+        self.foreground = self.image_label("images/foreground.png")
+        width = 300
+        height = int(self.bodies.height() / self.bodies.width() * width)
+        x, y = 0, 0
+        self.setGeometry(x, y, width, height)
+        self.animation = QPropertyAnimation(self.heads, b"y")
+        self.animation.setEasingCurve(QEasingCurve.Type.InOutCubic)
+        self.animation.setEndValue(self.heads)
+        self.animation.setDuration(1500)
+        self.animation.start()
+
+    def image_label(self, path):
+        pixmap = QPixmap(path)
+        label = QLabel(self)
+        label.setPixmap(pixmap)
+        return label
+
+    def paintEvent(self, _event) -> None:
+        painter = QPainter(self)
+        painter.drawPixmap(self.rect(), self.bodies)
+        painter.drawPixmap(self.rect(), self.heads)
+        painter.drawPixmap(self.rect(), self.foreground)
 
     def mousePressEvent(self, event):
         self.old_pointer_position = event.pos()
@@ -24,9 +44,6 @@ class Fnoof(QMainWindow):
         self.move(difference)
 
 app = QApplication([])
-
 window = Fnoof()
-
 window.show()
-
 app.exec()
