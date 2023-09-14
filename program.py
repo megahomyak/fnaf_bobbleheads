c573ee0c589d095e8861284ea24f31ed38f05f35
megahomyak <g.megahomyak@gmail.com>
2023-09-14 11:18:19 +0500

    Experimenting with resizing the image

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
 from PyQt6.QtCore import QEasingCurve, QPoint, QPropertyAnimation, Qt
 from PyQt6.QtGui import QPainter, QPixmap
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QSizePolicy
 
 class Fnoof(QMainWindow):
 
@@ -10,28 +10,28 @@ class Fnoof(QMainWindow):
         # self.setWindowFlag(Qt.WindowType.X11BypassWindowManagerHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.old_pointer_position = None
-        self.bodies = QPixmap("images/bodies.png")
-        self.heads = QPixmap("images/heads.png")
-        self.foreground = QPixmap("images/foreground.png")
-        width = 300
-        height = int(self.bodies.height() / self.bodies.width() * width)
+        image_size = QPixmap("images/bodies.png").size()
+        self.win_width = 300
+        self.win_height = int(image_size.height() / image_size.width() * self.win_width)
+        self.bodies = self.image_label("images/bodies.png")
+        self.heads = self.image_label("images/heads.png")
+        self.foreground = self.image_label("images/foreground.png")
         x, y = 0, 0
-        self.setGeometry(x, y, width, height)
-        self.setProperty("heads_y", 0)
-        self.animation = QPropertyAnimation(self, b"heads_y")
+        self.setGeometry(x, y, self.win_width, self.win_height)
+        self.animation = QPropertyAnimation(self.heads, b"pos")
         self.animation.setEasingCurve(QEasingCurve.Type.InOutCubic)
-        self.animation.setEndValue(-100)
+        self.animation.setEndValue(self.heads.pos() + QPoint(0, 100))
         self.animation.setDuration(1500)
         self.animation.start()
 
-    def paintEvent(self, _event) -> None:
-        painter = QPainter(self)
-        heads_rect = self.rect()
-        print(self.property("heads_y"))
-        heads_rect.setY(heads_rect.y() + self.property("heads_y"))
-        painter.drawPixmap(self.rect(), self.bodies)
-        painter.drawPixmap(heads_rect, self.heads)
-        painter.drawPixmap(self.rect(), self.foreground)
+    def image_label(self, path):
+        label = QLabel(self)
+        label.setGeometry(0, 0, self.win_width, self.win_height)
+        pixmap = QPixmap(path).scaled(label.size(), Qt.AspectRatioMode.KeepAspectRatio)
+        label.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
+        pixmap = QPixmap(path)
+        label.setPixmap(pixmap)
+        return label
 
     def mousePressEvent(self, event):
         self.old_pointer_position = event.pos()
