1d0b92346926a46f66aa4ba591f31e085de89e11
megahomyak <g.megahomyak@gmail.com>
2023-09-14 10:40:36 +0500

    Repaint is only called once, so it does not work either

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
 from PyQt6.QtCore import QEasingCurve, QPoint, QPropertyAnimation, Qt
 from PyQt6.QtGui import QPainter, QPixmap
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 class Fnoof(QMainWindow):
 
@@ -10,24 +10,28 @@ class Fnoof(QMainWindow):
         # self.setWindowFlag(Qt.WindowType.X11BypassWindowManagerHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.old_pointer_position = None
-        self.bodies = self.image_label("images/bodies.png")
-        self.heads = self.image_label("images/heads.png")
-        self.foreground = self.image_label("images/foreground.png")
+        self.bodies = QPixmap("images/bodies.png")
+        self.heads = QPixmap("images/heads.png")
+        self.foreground = QPixmap("images/foreground.png")
         width = 300
         height = int(self.bodies.height() / self.bodies.width() * width)
         x, y = 0, 0
         self.setGeometry(x, y, width, height)
-        self.animation = QPropertyAnimation(self.heads, b"pos")
+        self.setProperty("heads_y", 0)
+        self.animation = QPropertyAnimation(self, b"heads_y")
         self.animation.setEasingCurve(QEasingCurve.Type.InOutCubic)
-        self.animation.setEndValue(self.heads.pos() + QPoint(0, -100))
+        self.animation.setEndValue(-100)
         self.animation.setDuration(1500)
         self.animation.start()
 
-    def image_label(self, path):
-        pixmap = QPixmap(path)
-        label = QLabel(self)
-        label.setPixmap(pixmap)
-        return label
+    def paintEvent(self, _event) -> None:
+        painter = QPainter(self)
+        heads_rect = self.rect()
+        print(self.property("heads_y"))
+        heads_rect.setY(heads_rect.y() + self.property("heads_y"))
+        painter.drawPixmap(self.rect(), self.bodies)
+        painter.drawPixmap(heads_rect, self.heads)
+        painter.drawPixmap(self.rect(), self.foreground)
 
     def mousePressEvent(self, event):
         self.old_pointer_position = event.pos()
